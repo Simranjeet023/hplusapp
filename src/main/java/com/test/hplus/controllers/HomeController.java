212d47bc47e4bd88869fa46dbe7dc58d7076cd7c
Simranjeet023 <69615701+Simranjeet023@users.noreply.github.com>
2021-08-03 15:59:24 -0400

    Initial Commit

---
+++ src/main/java/com/test/hplus/controllers/HomeController.java
@@ -0,0 +1,52 @@
+package com.test.hplus.controllers;
+
+import com.test.hplus.beans.Login;
+import com.test.hplus.beans.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String goHome(){
+        System.out.println("In home controller");
+        return "index";
+    }
+
+    @GetMapping("/goToSearch")
+    public String goToSearch(){
+        System.out.println("Going to search page");
+        return "search";
+    }
+
+    @GetMapping("/goToLogin")
+    public String goToLogin(){
+        System.out.println("Going to login page");
+        return "login";
+    }
+
+    @GetMapping("/goToRegistration")
+    public String goToRegistration(){
+        System.out.println("Going to register page");
+        return "register";
+    }
+
+//    @ModelAttribute("newuser")
+//    public User gerDefaultUser(){
+//        return new User();
+//    }
+//
+//    @ModelAttribute("genderItems")
+//    public List<String> gerGenderItems(){
+//        return Arrays.asList(new String[]{"Male", "Female", "Other"});
+//    }
+//
+//    @ModelAttribute("login")
+//    public Login gerDefaultLogin(){
+//        return new Login();
+//    }
+}
---
+++ src/main/java/com/test/hplus/exceptions/ApplicationExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.test.hplus.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler({ApplicationException.class, AsyncRequestTimeoutException.class})
+    public String handleException(){
+        System.out.println("In global exception handler");
+        return "error";
+    }
+
+    @ExceptionHandler(LoginFailureException.class)
+    public ResponseEntity handleLoginFailure(LoginFailureException ex){
+       // System.out.println("In global exception handler");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+}
